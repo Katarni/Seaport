ce626e5752747eb114c5d5b922f14b4bac68ec3e
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-10-07 22:22:28 +0300

    includes to header

--- header.h
+++ header.h
@@ -7,5 +7,7 @@
 #include <KatLib.h>
 #include <iostream>
 #include <vector>
+#include <random>
+#include "manager/Manager.h"
 
 const sf::Color kBlue = sf::Color(31, 184, 193), kLightBlue = sf::Color(16, 204, 224);
